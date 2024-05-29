7c8406aedc6802369a7f48eb333e92e0268726d8
ACIPLF0002 <acuitilabs@gmail.com>
2024-05-29 11:09:27 +0530

    updated operator file to automate deployment and gave permission to operator

---
+++ api/v1alpha1/constants.go
@@ -0,0 +1,8 @@
+// api/v1alpha1/constants.go
+
+package v1alpha1
+
+const (
+	FAILED  = "Failed"
+	SUCCESS = "Success"
+)
--- controllers/test_controller.go
+++ controllers/test_controller.go
@@ -1,23 +1,8 @@
-/*
-Copyright 2024 Dhiraj Bhattad.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package controllers
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	appsv1 "k8s.io/api/apps/v1"
@@ -40,15 +25,6 @@ type TestReconciler struct {
 //+kubebuilder:rbac:groups=myworkspot.in.myworkspot.in,resources=tests/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=myworkspot.in.myworkspot.in,resources=tests/finalizers,verbs=update
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO: Modify the Reconcile function to compare the state specified by
-// the Test object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.10.0/pkg/reconcile
 func (r *TestReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := log.FromContext(ctx)
 	log.Info("Reconcile called", "Request.Namespace", req.Namespace, "Request.Name", req.Name)
@@ -58,37 +34,54 @@ func (r *TestReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.
 	if err != nil {
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
+
 	startTime := test.Spec.Start
 	endTime := test.Spec.End
 	replicas := test.Spec.Replicas
 
 	currentHour := time.Now().UTC().Hour()
+	log.Info(fmt.Sprintf("currentTime %d", currentHour))
 
 	if currentHour >= startTime && currentHour <= endTime {
-		for _, deploy := range test.Spec.Deployment {
-			deployment := &appsv1.Deployment{}
-			err := r.Get(ctx, types.NamespacedName{
-				Namespace: deploy.Namespace,
-				Name:      deploy.Name,
-			}, deployment)
-			if err != nil {
-				return ctrl.Result{}, err
-			}
+		err = scaleDeployment(test, r, ctx, replicas)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+	}
 
-			if *deployment.Spec.Replicas != replicas {
-				deployment.Spec.Replicas = &replicas
-				err := r.Update(ctx, deployment)
-				if err != nil {
-					return ctrl.Result{}, err
-				}
+	return ctrl.Result{RequeueAfter: 30 * time.Second}, nil
+}
+
+func scaleDeployment(test *myworkspotinv1alpha1.Test, r *TestReconciler, ctx context.Context, replicas int32) error {
+	for _, deploy := range test.Spec.Deployment {
+		dep := &appsv1.Deployment{}
+		err := r.Get(ctx, types.NamespacedName{
+			Namespace: deploy.Namespace,
+			Name:      deploy.Name,
+		}, dep)
+		if err != nil {
+			return err
+		}
+
+		if *dep.Spec.Replicas != replicas {
+			dep.Spec.Replicas = &replicas
+			err = r.Update(ctx, dep)
+			if err != nil {
+				test.Status.Status = myworkspotinv1alpha1.FAILED
+				return err
 			}
 		}
 	}
 
-	return ctrl.Result{RequestAfter: time.Duration(30 * time.Second)}, nil
+	test.Status.Status = myworkspotinv1alpha1.SUCCESS
+	err := r.Status().Update(ctx, test)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
-// SetupWithManager sets up the controller with the Manager.
 func (r *TestReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&myworkspotinv1alpha1.Test{}).
